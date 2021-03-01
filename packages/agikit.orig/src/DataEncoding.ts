313ab0c991e4ee698031576e1f1c5f667d432f37
Nat Budin <natbudin@gmail.com>
2021-02-28 22:31:24 -0800

    Build OBJECT and WORDS.TOK; fix logic encoding bug

--- packages/agikit.orig/src/DataEncoding.ts
+++ packages/agikit.orig/src/DataEncoding.ts
@@ -1,3 +1,7 @@
 export function encodeUInt16LE(int: number): number[] {
   return [int & 0xff, (int & 0xff00) >> 8];
 }
+
+export function encodeUInt16BE(int: number): number[] {
+  return [(int & 0xff00) >> 8, int & 0xff];
+}
